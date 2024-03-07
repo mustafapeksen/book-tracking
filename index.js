f581c31c51ce0aafe7d965be162f729eef4153e5
mustafapeksen <mustafapeksen04@gmail.com>
2024-03-07 13:29:42 +0300

    Add edit.ejs file and write the necessary codes for editing in the backend

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ db.connect(); // Connecting to the PostgreSQL database
 // Handling GET requests to the root endpoint
 app.get("/", async (req, res) => {
     // Querying the database to retrieve book information including title, ISBN, author, etc.
-    const result = await db.query("SELECT books.book_title,books.isbn,books.author,books.id , scores.date_read,scores.like_score, notes.summary FROM books JOIN scores ON books.id = scores.book_id JOIN notes ON books.id = notes.book_id ORDER BY scores.like_score DESC");
-
+    const result = await db.query("SELECT books.book_title,books.isbn,books.author,books.id , scores.date_read,scores.like_score, notes.summary "+
+    "FROM books JOIN scores ON books.id = scores.book_id JOIN notes ON books.id = notes.book_id ORDER BY scores.like_score DESC");
+    
     const information = result.rows; // Storing the retrieved data from the database
 
     // Rendering the 'index.ejs' template with the retrieved book information
@@ -31,17 +32,13 @@ app.get("/", async (req, res) => {
 });
 
 // Handling GET requests to the '/book/:name' endpoint
-app.get("/book/:name", async (req, res) => {
+app.get("/book/:id", async (req, res) => {
     try {
-        const bookName = req.params.name; // Get the book name from the URL parameter
+        const bookId = req.params.id; // Get the book name from the URL parameter
 
         // Query the database to retrieve detailed information about a specific book
-        const result = await db.query("SELECT books.book_title, books.isbn, books.author, scores.date_read, scores.like_score, notes.summary, notes.note FROM books JOIN scores ON books.id = scores.book_id JOIN notes ON books.id = notes.book_id WHERE books.book_title = $1", [bookName]);
-
-        // If the book is not found in the database, send an appropriate message to the client
-        if (result.rows.length === 0) {
-            return res.render("book_not_found.ejs", { bookName: bookName });
-        }
+        const result = await db.query("SELECT books.book_title,books.id, books.isbn, books.author, scores.date_read, scores.like_score, notes.summary, notes.note" +
+            " FROM books JOIN scores ON books.id = scores.book_id JOIN notes ON books.id = notes.book_id WHERE books.id = $1", [bookId]);
 
         const information = result.rows[0]; // Store the retrieved book information
 
@@ -99,8 +96,6 @@ function capitalizeEveryWord(sentence) {
 
 
 app.post("/add", async (req, res) => {
-    console.log(req.body);
-
     const bookTitle = capitalizeEveryWord(req.body.book_title);
     const isbn = capitalizeEveryWord(req.body.isbn);
     const author = capitalizeEveryWord(req.body.author);
@@ -115,6 +110,87 @@ app.post("/add", async (req, res) => {
     await db.query("INSERT INTO scores VALUES($1,$2,$3)", [likeScore, bookId, dateRead]);
     res.redirect("/");
 });
+
+app.get("/edit/:id", async (req, res) => {
+    try {
+        const bookId = req.params.id; // Get the book name from the URL parameter
+
+        // Query the database to retrieve detailed information about a specific book
+        const result = await db.query("SELECT books.book_title,books.id, books.isbn, books.author, scores.date_read, scores.like_score, notes.summary, notes.note" +
+            " FROM books JOIN scores ON books.id = scores.book_id JOIN notes ON books.id = notes.book_id WHERE books.id = $1", [bookId]);
+
+
+        const information = result.rows[0]; // Store the retrieved book information
+
+        // Split the summary and note into paragraphs based on double line breaks
+        const summaryParagraphs = information.summary.split('\n\n');
+        const noteParagraphs = information.note.split('\n\n');
+
+        // Fetch book cover information from the Open Library Covers API using ISBN
+        let olid;
+        try {
+            const resultJson = await axios.get(`https://covers.openlibrary.org/b/isbn/${information.isbn}.json`);
+            const data = resultJson.data; // Store the retrieved data
+            olid = data.olid;
+        } catch (error) {
+            console.error("An error occurred during the API request:", error);
+            // If there is an error with the API request, olid can be left empty or assigned a default value
+        }
+        // Render the 'book.ejs' template with book details, summary, note, and cover information
+        res.render("edit.ejs", {
+            books: information,
+            summaryParagraphs: summaryParagraphs,
+            noteParagraphs: noteParagraphs,
+            olid: olid
+        });
+    } catch (error) {
+        // Handle any errors that occur during the process
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.post("/edit/:id", async (req, res) => {
+    try {
+        const bookTitle = capitalizeEveryWord(req.body.book_title);
+        const isbn = capitalizeEveryWord(req.body.isbn);
+        const author = capitalizeEveryWord(req.body.author);
+        const summary = req.body.summary;
+        const note = req.body.note;
+        const likeScore = req.body.like_score;
+        const dateRead = req.body.date_read;
+        const bookId = req.params.id; // Parametreyi düzeltin
+
+        // Query the database to retrieve detailed information about a specific book
+        const result = await db.query("SELECT books.book_title, books.isbn, books.author FROM books WHERE id = $1", [bookId]);
+        const information = result.rows[0]; // Store the retrieved book information
+
+        let olid = ''; // Default bir değer atayın
+
+        // ISBN varsa API isteğini yapın
+        if (information && information.isbn) {
+            try {
+                const resultJson = await axios.get(`https://covers.openlibrary.org/b/isbn/${information.isbn}.json`);
+                const data = resultJson.data; // Store the retrieved data
+                olid = data.olid;
+            } catch (error) {
+                console.error("An error occurred during the API request:", error);
+            }
+        }
+
+        // Veritabanını güncelleyin
+        await db.query("UPDATE books SET book_title = $1, isbn = $2, author = $3 WHERE id = $4", [bookTitle, isbn, author, bookId]);
+        await db.query("UPDATE notes SET summary = $1, note = $2 WHERE book_id = $3", [summary, note, bookId]);
+        await db.query("UPDATE scores SET like_score = $1, date_read = $2 WHERE book_id = $3", [likeScore, dateRead, bookId]);
+        res.redirect(`/book/${bookId}`);
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 
 app.post("/delete", async (req, res) => {
     const deleteId = req.body['book-id'];
